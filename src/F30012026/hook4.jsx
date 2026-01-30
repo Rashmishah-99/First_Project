56109ffb67993c2e3b274cacc96f12a00f7aeb8b
Rashmi Shah <rs8893905@gmail.com>
2026-01-30 13:56:55 +0530

    add update btn and update functionality

--- src/F30012026/hook4.jsx
+++ src/F30012026/hook4.jsx
@@ -1,71 +1,97 @@
-// create a react functional component that will 
-// accept first name , last name, mobile number 
+// create a react functional component that will
+// accept first name, last name and mobile number
 // add a button to submit record
-//  display first name in list with 2 buttons 
-// view and delete , when user click on view 
-// it will toggle display of last name and mobile number 
-// when user click on delete it will remove the record
-import React, { useState } from 'react';
-export default function ContactLists(){
-    const [contacts, setContacts] = useState([]);
-    const [firstName, setFirstName] = useState("");
-    const [lastName, setLastName] = useState("");
-    const [mobile, setMobile] = useState("");
-    const [viewIndex, setViewIndex] = useState(null);
-    const addContact = () => {
-        setContacts([...contacts, { firstName, lastName, mobile }]);
-        setFirstName("");
-        setLastName("");
-        setMobile("");
-    };
-    const deleteContact = (index) => {
-        setContacts(contacts.filter((_, i) => i !== index));
-        if(viewIndex === index){
-            setViewIndex(null);
-        }
-    };
-    const viewContact = (index) => {
-        setViewIndex(index);
-    };
-    return (
-        <>
-            <h1>Contacts</h1>
-            <input
-                type="text"
-                placeholder="First Name"
-                value={firstName}
-                onChange={(e) => setFirstName(e.target.value)}
-            />
-            <input
-                type="text"
-                placeholder="Last Name"
-                value={lastName}
-                onChange={(e) => setLastName(e.target.value)}
-            />
-            <input
-                type="number"
-                placeholder="Mobile"
-                value={mobile}
-                onChange={(e) => setMobile(e.target.value)}
-            />
-            <button onClick={addContact}>Add Contact</button>
-            <ul>
-                {contacts.map((contact, index) => (
-                    <li key={index}>
-                        {contact.firstName}
-                        <button onClick={() => viewContact(index)}>View</button>
-                        <button onClick={() => deleteContact(index)}>Delete</button>
-                        {viewIndex === index && (
-                            <>
-                                <p>{contact.lastName}</p>
-                                <p>{contact.mobile}</p>
-                            </>
-                        )}
-                    </li>
-                ))}
-            </ul>
-        </>
-    );
+// display first name in list with 2 buttons
+// view and delete,  when user click on view
+// it will toggle display of last name and mobile number
+//  when user click on delete it will remove the record
+import React, { useState } from "react";
+export default function HookExampl4() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [mobile, setMobile] = useState("");
+  const [con_list, setConList] = useState([]);
+    const [id1, setId1] = useState("");
 
+  const addContact = () => {
+    const newContact = {
+      id: Date.now(),
+      fname: firstName,
+      lname: lastName,
+      contact: mobile,
+      display: false,
+    };
+    setConList([...con_list, newContact]);
+    setFirstName("");
+    setLastName("");
+    setMobile("");
+  };
+  const deleteContact = (id) => {
+    setConList(con_list.filter((x) => x.id !== id));
+  };
+  const toggleDisplay = (id) => {
+    setConList(
+      con_list.map((y) => (y.id === id ? { ...y, display: !y.display } : y)),
+    );
+  };
+
+  const setRecord = (id) => {
+     setId1(id);
+      setFirstName(con_list.find((x) => x.id === id).fname);
+      setLastName(con_list.find((x) => x.id === id).lname);
+      setMobile(con_list.find((x) => x.id === id).contact);
+  }
+  const updateRecord = () => {
+    setConList(
+      con_list.map((x) => (x.id === id1 ? { ...x, fname: firstName, lname: lastName, contact: mobile } : x)),
+    );
+    setFirstName("");
+    setLastName("");
+    setMobile("");
+    setId1("");
+  };
+  
+  return (
+    <>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        value={lastName}
+        onChange={(e) => setLastName(e.target.value)}
+      />
+      <br />
+      <input
+        type="text"
+        value={mobile}
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <br />
+      <button onClick={() => addContact()}>Add</button>
+      <button onClick={() => updateRecord()}>Update</button>
+      <ul>
+        {con_list.map((x) => (
+          <li key={x.id}>
+            {x.fname}
+            <button onClick={() => toggleDisplay(x.id)}>View</button>
+            <button onClick={() => deleteContact(x.id)}>Delete</button>
+            <div style={{ display: x.display ? "" : "none " }}>
+              {x.lname} {" --> "} {x.contact}
+            </div>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
+// contact = [
+//   { id: 1, fname: "abc", lname: "xyz", contact: "1234", display: true },
+//   { id: 2, fname: "abc1", lname: "xyz1", contact: "12341", display: false },
+//   { id: 3, fname: "abc2", lname: "xyz2", contact: "12342", display: false },
+// ];
+// id: 1, fname: "abc", lname: "xyz", contact: "1234", display: true
